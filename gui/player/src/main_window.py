1bb887ccfe696cf094e1252a46e14e817131c070
SimonMaracine <simonmaracine@gmail.com>
2024-09-24 22:43:04 +0300

    Work on displaying the board

--- gui/player/src/main_window.py
+++ gui/player/src/main_window.py
@@ -4,7 +4,7 @@ class MainWindow(tk.Frame):
     def __init__(self, root: tk.Tk):
         super().__init__(root)
 
-        self._root = root
+        self._tk = root
         self._return_code = 0
 
         self._setup_widgets()
@@ -14,21 +14,56 @@ class MainWindow(tk.Frame):
 
     def _setup_widgets(self):
         # self.root.option_add("*tearOff", False)
-        self._root.title("Checkers Player")
-        self._root.geometry("768x432")
-        self._root.protocol("WM_DELETE_WINDOW", self._on_window_closed)
-        self._root.minsize(512, 288)
+        self._tk.title("Checkers Player")
+        self._tk.geometry("768x432")
+        self._tk.protocol("WM_DELETE_WINDOW", self._on_window_closed)
+        self._tk.minsize(512, 288)
 
-        self.pack(fill="both", expand=True, padx=20, pady=20)
+        self.pack(fill="both", expand=True)
 
         # TODO temp
-        self.configure(bg="red")
+        self.config(bg="blue")
 
         self._setup_widgets_board()
+        self._setup_widgets_right()
+
+        # Do this after all widgets are configured
+        self.bind("<Configure>", self._on_window_resized)
 
     def _setup_widgets_board(self):
-        tk.Label(self, text="Hello, world!").pack()
+        size = self._calculate_board_size()
+
+        self._cvs_board = tk.Canvas(self, width=size, height=size, background="gray75")
+        self._cvs_board.pack(expand=True)
+
+        width = self._cvs_board["width"]
+        square_size = float(width) / 8
+
+        for i in range(8):
+            for j in range(8):
+                self._cvs_board.create_rectangle(i * square_size, j * square_size, i * square_size + square_size, j * square_size + square_size, fill="red" if (i + j) % 2 != 0 else "black")
+
+    def _setup_widgets_right(self):
+        pass
+
+    def _on_window_resized(self, event):
+        size = self._calculate_board_size((event.width, event.height))
+        self._cvs_board.config(width=size, height=size)
 
     def _on_window_closed(self):
         self.destroy()
-        self._root.destroy()
+        self._tk.destroy()
+
+    def _calculate_board_size(self, window_size=None) -> int:
+        if window_size is None:
+            self._tk.update()
+            geometry = self._tk.geometry()
+            size = geometry.split("+")[0].split("x")
+
+            window_width = int(size[0])
+            window_height = int(size[1])
+        else:
+            window_width = window_size[0]
+            window_height = window_size[1]
+
+        return min(window_width, window_height) - 40
