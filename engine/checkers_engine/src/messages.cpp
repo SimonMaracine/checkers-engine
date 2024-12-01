9b47467aa930b429078265e91c4a89b300f870af
SimonMaracine <simonmaracine@gmail.com>
2024-12-01 17:16:00 +0200

    Increment engine version
    Should have done this already, arrrghh.

--- engine/checkers_engine/src/messages.cpp
+++ engine/checkers_engine/src/messages.cpp
@@ -141,7 +141,7 @@ namespace messages {
     void name() {
         std::lock_guard<std::mutex> lock {g_mutex};
 
-        std::cout << "NAME " << "checkers|4.0" << std::endl;
+        std::cout << "NAME " << "checkers|5.0" << std::endl;
     }
 
     void board(const game::Position& position) {
