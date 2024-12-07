82b667bb7a7bf2421ffdf6373cc4bbc74ac27d2f
SimonMaracine <simonmaracine@gmail.com>
2024-12-07 14:27:54 +0200

    Fix search_perf test

--- engine/tests/src/search_perf.cpp
+++ engine/tests/src/search_perf.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <thread>
 
 #include <loop.hpp>
 
@@ -7,10 +8,17 @@ static std::string read_next_input() {
 
     static const std::string commands[] {
         "INIT",
-        "SETPARAMETER depth 15",
+        "SETPARAMETER depth 12",
+        "SETPARAMETER max_depth 12",
         "GO",
         "QUIT"
     };
+
+    using namespace std::literals;
+
+    if (commands[index] == "QUIT") {
+        std::this_thread::sleep_for(2s);
+    }
 
     return commands[index++];
 }
